cf0481a55c35101139fccb75e05e44ec583ad547
AnastasiaPanysh <nastya.panysh@gmail.com>
2023-06-26 23:40:03 +0300

    added client

--- client/src/components/Header/Header.js
+++ client/src/components/Header/Header.js
@@ -4,21 +4,22 @@ import style from './style.module.css'
 function Header() {
     return (
         <div className={style.wrapper}>
-        <div className={style.content}>
+            <div className={style.content}>
 
-            <h1>Hschool</h1>
+                <h1>Hschool</h1>
 
-            <nav>
-                <div className={style.login}>
-                    <Link to='/auth'>Login →</Link>
-                </div>
-                <div className={style.sign}>
-                    <Link to='/reg'>Sign Up</Link>
-                </div>
-            </nav>
+                <nav>
+                    <Link to='/auth'>
+                        <div className={style.login}>Login →</div>
+                    </Link>
+                    
+                    <Link to='/reg'>
+                        <div className={style.sign}>Sign Up</div>
+                    </Link>
+                </nav>
 
 
-        </div>
+            </div>
         </div>
     )
 }
--- client/src/pages/HomePage/HomePage.js
+++ client/src/pages/HomePage/HomePage.js
@@ -1,10 +1,43 @@
 import Header from "../../components/Header/Header"
+import style from './style.module.css'
 
 function HomePage() {
     return (
         <>
             <Header></Header>
 
+            <div className={style.preview}>
+                <div className={style.previewContent}>
+                    <p className={style.textPlatform}>E-COURSE PLATFORM</p>
+                    <h1>Learning and
+                        teaching online,
+                        made easy.</h1>
+                    <p className={style.textAdditional}>Any subject, in any language, on any device, for all ages!</p>
+                    <div className={style.btn} >About platform</div>
+                    <div className={style.statictics}>
+                        <p className={style.studentsCount}><span className={style.lightning}></span>600 <span>+</span></p>
+                        <p className={style.students}>Students</p>
+                    </div>
+                </div>
+                <div className={style.manImg}></div>
+            </div>
+
+            <div className={style.learnBlock}>
+                <div className={style.learnBlockWidth }>
+                    <div className={style.learnImg}></div>
+                    <div className={style.learnBlockContent}>
+                        <h2>Learn a language in a playful way</h2>
+                        <p>Make learning programming languages more fun with mini-games</p>
+                        <div className={style.wrapperIcons}>
+                            <div className={style.sprint}></div>
+                            <div className={style.tasks}></div>
+                        </div>
+
+                    </div>
+                </div>
+            </div>
+            <div></div>
+            <div></div>
 
         </>
     )
