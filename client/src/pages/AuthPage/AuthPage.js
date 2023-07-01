1799a3b9791e033afbb3ef259463f38a7ed09a7d
AnastasiaPanysh <nastya.panysh@gmail.com>
2023-07-01 15:33:05 +0300

    added AuthPage

--- client/src/pages/AuthPage/AuthPage.js
+++ client/src/pages/AuthPage/AuthPage.js
@@ -1,10 +1,33 @@
+import Footer from '../../components/Footer/Footer'
+import Header from '../../components/Header/Header'
 import style from './style.module.scss'
 
-function AuthPage(){
-    return(
+function AuthPage() {
+    return (
         <>
-        
+            <Header />
+            <div className={style.wrapper}>
+                <div className={style.loginForm}>
+                    <h1>Login</h1>
+                    <div>
+                        <input placeholder='Placeholder text' />
+                    </div>
+
+                    <div>
+                        <input placeholder='Placeholder text' />
+                    </div>
+
+                    <div className={style.btn}>Login</div>
+                </div>
+
+
+                <div className={style.image}></div>
+
+            </div>
+
+            <Footer />
         </>
+
     )
 }
 
